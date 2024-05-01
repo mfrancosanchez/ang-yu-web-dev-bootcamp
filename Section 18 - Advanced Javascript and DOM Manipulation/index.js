076993e1ddcc1121ca4a042a21886dfbe95941e6
M. Franco Sanchez <misaelsanchez1842@gmail.com>
2024-04-30 23:30:58 -0400

    Section 18 exercise 1 completed

--- Section 18 - Advanced Javascript and DOM Manipulation/index.js
+++ Section 18 - Advanced Javascript and DOM Manipulation/index.js
@@ -1,1 +1,5 @@
-console.log("I'm working");
+
+document.querySelectorAll("button.drum").forEach((button, i) =>
+    button.addEventListener("click", function () {
+        alert(`Button ${button.innerText} (array# ${i}) got click`);
+    }));
