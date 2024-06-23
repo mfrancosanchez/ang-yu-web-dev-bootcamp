ab2277de4ca9fa7bb87d5ae1bfc2c80dcf9be693
M. Franco Sanchez <misaelsanchez1842@gmail.com>
2024-06-22 22:16:42 -0400

    Add: start game feature

--- Section 20 - Boss Level Challenge 2 - Simon Game/game.js
+++ Section 20 - Boss Level Challenge 2 - Simon Game/game.js
@@ -1,6 +1,7 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
 
 function nextSequence() {
     let randomNumber = Math.round(Math.random() * 3);
@@ -8,6 +9,8 @@ function nextSequence() {
     gamePattern.push(randomChosenColor);
     playSound(randomChosenColor);
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+    level++;
+    $(`#level-title`).text(`Level ${level}`);
 }
 
 $(".btn").each(function (i, button) {
@@ -32,6 +35,13 @@ function animatePress(currentColor) {
     }, 100);
 }
 
+$(document).on("keyup", function(){
+    let hasGameStarted = false;
+    if(!hasGameStarted){
+        hasGameStarted = true;
+        nextSequence();
+    }
+})
 
 // return `randomNumber ${randomNumber},
 // randomChosenColor ${randomChosenColor},
