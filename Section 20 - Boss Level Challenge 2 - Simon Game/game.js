932fc7ef0c234c7e0229b875ca2bf9fd83c0a8b1
M. Franco Sanchez <misaelsanchez1842@gmail.com>
2024-06-30 16:35:08 -0400

    feat: added game logic for checking user answers

--- Section 20 - Boss Level Challenge 2 - Simon Game/game.js
+++ Section 20 - Boss Level Challenge 2 - Simon Game/game.js
@@ -2,46 +2,76 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
+let hasGameStarted = false;
 
+// trigger next sequence
 function nextSequence() {
     let randomNumber = Math.round(Math.random() * 3);
-    let randomChosenColor = buttonColors[randomNumber];
+    const randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     playSound(randomChosenColor);
     $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+
+    // h1 lavel change
     level++;
     $(`#level-title`).text(`Level ${level}`);
+
+    // console.log(`user ${userClickedPattern}`);
+    console.log(`game ${gamePattern}`);
 }
 
+// make the buttons clickable | user selection
 $(".btn").each(function (i, button) {
-    $(button).on("click", function () {
-        let userChosenColor = $(this).attr("id");
+    $(button).on("click", () => {
+        const userChosenColor = $(this).attr("id");
         userClickedPattern.push(userChosenColor);
+        checkAnswer(userClickedPattern.length - 1);
         playSound(userChosenColor);
         animatePress(userChosenColor);
     });
 });
 
+// start the game on key press
+$(document).on("keyup", () => {
+    if (!hasGameStarted) {
+        nextSequence();
+        hasGameStarted = true;
+    }
+})
+
+// play sound method
 function playSound(name) {
-    let sound = new Audio(`sounds/${name}.mp3`);
-    let wrongSound = new Audio(`sounds/wrong.mp3`);
+    const sound = new Audio(`sounds/${name}.mp3`);
+    const wrongSound = new Audio(`sounds/wrong.mp3`);
     return name ? sound.play() : wrongSound.play();
 }
 
+// button press animation
 function animatePress(currentColor) {
     $(`#${currentColor}`).addClass("pressed");
-    setTimeout(function () {
+    setTimeout(() => {
         $(`#${currentColor}`).removeClass("pressed");
     }, 100);
 }
 
-$(document).on("keyup", function(){
-    let hasGameStarted = false;
-    if(!hasGameStarted){
-        hasGameStarted = true;
-        nextSequence();
+// checking answers function
+function checkAnswer(currentLevel) {
+    // console.log(`user ${userClickedPattern}`);
+    // console.log(`game ${gamePattern}`);
+
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log('success');
+        if (userClickedPattern.length - 1 == gamePattern.length - 1) {
+            setTimeout(() => {
+                nextSequence();
+                userClickedPattern = [];
+            }, 1000);
+        }
+    } else {
+        console.log('wrong');
+        // console.log(`user ${userClickedPattern} || game ${gamePattern}`);
     }
-})
+}
 
 // return `randomNumber ${randomNumber},
 // randomChosenColor ${randomChosenColor},
