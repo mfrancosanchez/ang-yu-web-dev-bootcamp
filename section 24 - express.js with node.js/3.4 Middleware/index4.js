d738d651fa967f72f57bbc1fc0e31c0bd922c3d7
M.Franco <misaelsanchez1842@gmail.com>
2024-12-16 22:58:25 -0400

    1.	Update project name and add morgan dependency in package-lock.json 	2.	Integrate morgan middleware for logging HTTP requests in index2.js 	3.	Add custom logger middleware to log request method and URL in index3.js 	4.	Implement Band Name Generator with body-parser and form handling in index4.js

--- section 24 - express.js with node.js/3.4 Middleware/index4.js
+++ section 24 - express.js with node.js/3.4 Middleware/index4.js
@@ -1,7 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+let bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandName = `${req.body.street} ${req.body.pet}`;
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.post("/submit", (req, res) => {
+  // bandName = req.body.street + req.body.pet;
+  res.send(`<h1>Your band name is:</h1><h2>🫸 ${bandName}🤌 🫷</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
